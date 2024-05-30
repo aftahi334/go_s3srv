d4e3ca00dabd20818847a84f6b0a515100b7ab06
aftahi334 <aftahi334@gmail.com>
2024-05-30 15:28:15 -0500

    run multiple servers at one time

--- main.go
+++ main.go
@@ -1,36 +1,77 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
-	"os"
-	//"rsc.io/quote"
-	//"github.com/gorilla/mux"
 )
 
+type keyServ string
+
+const keyServAddr = keyServ("serverAddr")
+
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
+	ctx := r.Context()
+	fmt.Printf("%s: got / request\n", ctx.Value(keyServAddr))
 	io.WriteString(w, "This is my website!\n")
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request\n")
+	ctx := r.Context()
+	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServAddr))
 	io.WriteString(w, "Hello HTTP!\n")
 }
 func main() {
-	//fmt.Println(quote.Go())
-	http.HandleFunc("/", getRoot)
-	http.HandleFunc("/hello", getHello)
 
-	err := http.ListenAndServe("127.0.0.1:3333", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", getRoot)
+	mux.HandleFunc("/hello", getHello)
 
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("server closed\n")
-	} else if err != nil {
-		fmt.Printf("error starting server: %s\n", err)
-		os.Exit(1)
+	ctx, cancelCtx := context.WithCancel(context.Background())
+
+	serverOne := &http.Server{
+		Addr:    "127.0.0.1:3333",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServAddr, l.Addr().String())
+			return ctx
+		},
 	}
+
+	serverTwo := &http.Server{
+		Addr:    "127.0.0.1:4444",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServAddr, l.Addr().String())
+			return ctx
+		},
+	}
+
+	go func() {
+		err := serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server one closed\n")
+		} else if err != nil {
+			fmt.Printf("error starting for server one: %s\n", err)
+		}
+		cancelCtx()
+
+	}()
+
+	go func() {
+		err := serverTwo.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("server two closed\n")
+		} else if err != nil {
+			fmt.Printf("error starting for server two: %s\n", err)
+		}
+		cancelCtx()
+
+	}()
+
+	<-ctx.Done()
 
 }
