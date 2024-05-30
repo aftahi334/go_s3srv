f6cce049eaa271bd9191a3946749cbbac8e36c05
aftahi334 <aftahi334@gmail.com>
2024-05-29 20:48:27 -0500

    simple http listener

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"rsc.io/quote"
+	"io"
+	"net/http"
+	"os"
+	//"rsc.io/quote"
+	//"github.com/gorilla/mux"
 )
 
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello HTTP!\n")
+}
 func main() {
-	fmt.Println(quote.Go())
+	//fmt.Println(quote.Go())
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe("127.0.0.1:3333", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+
 }
