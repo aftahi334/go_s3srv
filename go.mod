b19c9eaddbcf00b3f44fbf1a4422558649d8dc23
aftahi334 <aftahi334@gmail.com>
2024-06-05 19:28:50 -0500

    fix project path

--- go.mod
+++ go.mod
@@ -2,10 +2,4 @@ module github.com/aftahi334/go_s3srv
 
 go 1.22.3
 
-require rsc.io/quote v1.5.2
-
-require (
-	github.com/gorilla/mux v1.8.1 // indirect
-	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
-	rsc.io/sampler v1.3.0 // indirect
-)
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,77 +1,19 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"fmt"
-	"io"
-	"net"
 	"net/http"
+
+	"githubgo .com/gorilla/mux"
 )
 
-type keyServ string
-
-const keyServAddr = keyServ("serverAddr")
-
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	fmt.Printf("%s: got / request\n", ctx.Value(keyServAddr))
-	io.WriteString(w, "This is my website!\n")
+func sendFile(w http.ResponseWriter, r *http.Request) {
+	file := "sample.txt"
+	w.Header().Set("Content-Type", "application/octet-stream")
+	http.ServeFile(w, r, file)
 }
 
-func getHello(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServAddr))
-	io.WriteString(w, "Hello HTTP!\n")
-}
 func main() {
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", getRoot)
-	mux.HandleFunc("/hello", getHello)
-
-	ctx, cancelCtx := context.WithCancel(context.Background())
-
-	serverOne := &http.Server{
-		Addr:    "127.0.0.1:3333",
-		Handler: mux,
-		BaseContext: func(l net.Listener) context.Context {
-			ctx = context.WithValue(ctx, keyServAddr, l.Addr().String())
-			return ctx
-		},
-	}
-
-	serverTwo := &http.Server{
-		Addr:    "127.0.0.1:4444",
-		Handler: mux,
-		BaseContext: func(l net.Listener) context.Context {
-			ctx = context.WithValue(ctx, keyServAddr, l.Addr().String())
-			return ctx
-		},
-	}
-
-	go func() {
-		err := serverOne.ListenAndServe()
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Printf("server one closed\n")
-		} else if err != nil {
-			fmt.Printf("error starting for server one: %s\n", err)
-		}
-		cancelCtx()
-
-	}()
-
-	go func() {
-		err := serverTwo.ListenAndServe()
-		if errors.Is(err, http.ErrServerClosed) {
-			fmt.Printf("server two closed\n")
-		} else if err != nil {
-			fmt.Printf("error starting for server two: %s\n", err)
-		}
-		cancelCtx()
-
-	}()
-
-	<-ctx.Done()
-
+	r := mux.NewRouter()
+	r.HandleFunc("/sendfile", sendFile)
+	http.ListenAndServe("127.0.0.1:3333", r)
 }
