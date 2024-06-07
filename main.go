6986a5657148b5b62a7de003fee0b7d338600296
aftahi334 <aftahi334@gmail.com>
2024-06-07 14:25:30 -0500

    add getList to list the current directory

--- main.go
+++ main.go
@@ -10,11 +10,20 @@ import (
 )
 
 func main() {
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(pwd)
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/{filename}", getFile).Methods(http.MethodGet)
 	r.HandleFunc("/{filename}", sendFile).Methods(http.MethodPost)
 	r.HandleFunc("/{filename}", deleteFile).Methods(http.MethodDelete)
+	r.HandleFunc("/", getList).Methods(http.MethodGet)
 
 	fmt.Println("Starting server at port 3333")
 	http.ListenAndServe("127.0.0.1:3333", r)
@@ -76,4 +85,25 @@ func deleteFile(writer http.ResponseWriter, request *http.Request) {
 	}
 
 	writer.WriteHeader(http.StatusNoContent)
+}
+
+// curl command: curl -X GET http://127.0.0.1:3333/
+func getList(writer http.ResponseWriter, request *http.Request) {
+	// Get the list of files in the current directory
+	files, err := os.ReadDir(".")
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Write the list of files to the http.ResponseWriter
+	for _, file := range files {
+		_, err := writer.Write([]byte(file.Name() + "\n"))
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	writer.WriteHeader(http.StatusOK)
 }
