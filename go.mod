cf7ce71b7da3b9c1e7bf3bef6a8ae2c3a7aa032a
aftahi334 <aftahi334@gmail.com>
2024-06-07 00:57:26 -0500

    add get, send and delete file

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/aftahi334/go_s3srv
 
 go 1.22.3
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,19 +1,79 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
-func sendFile(w http.ResponseWriter, r *http.Request) {
-	file := "sample.txt"
-	w.Header().Set("Content-Type", "application/octet-stream")
-	http.ServeFile(w, r, file)
-}
-
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/sendfile", sendFile)
+
+	r.HandleFunc("/{filename}", getFile).Methods(http.MethodGet)
+	r.HandleFunc("/{filename}", sendFile).Methods(http.MethodPost)
+	r.HandleFunc("/{filename}", deleteFile).Methods(http.MethodDelete)
+
+	fmt.Println("Starting server at port 3333")
 	http.ListenAndServe("127.0.0.1:3333", r)
+}
+
+func getFile(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	filename := vars["filename"]
+
+	// Open the file for reading only
+	file, err := os.Open(filename)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	// Copy the file content to the http.ResponseWriter
+	_, err = io.Copy(writer, file)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	writer.WriteHeader(http.StatusOK)
+}
+
+func sendFile(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	filename := vars["filename"]
+
+	// Open a new file for writing only
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	// Copy the file from the request body to the new file
+	_, err = io.Copy(file, request.Body)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	writer.WriteHeader(http.StatusCreated)
+}
+
+func deleteFile(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	filename := vars["filename"]
+
+	// Delete the file
+	err := os.Remove(filename)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	writer.WriteHeader(http.StatusNoContent)
 }
